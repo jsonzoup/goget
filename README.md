7b6dbb87bf85c70870a6f5c5e97ebbe04eafa7f0
zoupengfei <pengfei.zou@pds-inc.com.cn>
2019-03-20 15:16:52 +0800

    golang get remote libs tool init.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # goget
 search the remote golang lib and get it
+在golang项目中查找依赖的远程库并下载
+
---
+++ goget.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"fmt"
+	"io/ioutil"
+	"io"
+	"bufio"
+	"regexp"
+	//"os/exec"
+)
+
+const  GITHUB = "github.com"
+// 不需要扫描的dir,分号分割
+const  NONE_SCAN_DIR = "views;static;vendor;docs;.idea;.mmwiki.session;conf;logs;data;Godeps;"
+// 项目根目录
+const PROJECT_ROOT_PATH = "D:/work/go/src/yourProject"
+
+// get "gitahub.com/xxx/yyy"
+func main() {
+	fmt.Println(PROJECT_ROOT_PATH)
+	remoteLibs := scanAll(PROJECT_ROOT_PATH)
+	fmt.Println("\n--------所有依赖的远程库的 go get 命令--------")
+	for _,rlib := range remoteLibs{
+		goget(rlib)
+	}
+}
+// 扫描所有的远程依赖包
+func scanAll(path string) []string{
+	remoteLibsMap := make(map[string]string)
+	scanFile(path,remoteLibsMap)
+	var remoteLibs []string
+	for k,_ := range remoteLibsMap {
+		remoteLibs = append(remoteLibs,k)
+	}
+	return remoteLibs
+}
+
+// 递归扫描指定工程下所有的go文件的代码
+func scanFile(path string,remoteLibsMap map[string]string){
+	files, _ := ioutil.ReadDir(path)
+	for _, fi := range files {
+		if fi.IsDir() {
+			// 过滤不需要扫描的dir
+			if !strings.Contains(NONE_SCAN_DIR,fi.Name()+";") {
+				scanFile(path + "/" + fi.Name(),remoteLibsMap)
+			}
+		} else {
+			// 只扫描go文件
+			if strings.Contains(fi.Name(), ".go") {
+				fmt.Println( path + "/" + fi.Name())
+				file, _ := os.Open(path + "/" + fi.Name())
+				buffer := bufio.NewReader(file)
+				for {
+					s, _, ok := buffer.ReadLine()
+					if ok == io.EOF {
+						break
+					}
+					result := matchRemoteLib(GITHUB,string(s));
+					if result != "" {
+					   remoteLibsMap[result]=""
+					}
+
+				}
+				file.Close()
+			}
+		}
+	}
+}
+
+// 逐行匹配获取远程lib
+func matchRemoteLib(remoteRepo string,codeLine string) string{
+	reg := regexp.MustCompile(`"(?P<remoteLib>`+remoteRepo+`/\S+/\S+){1}(/\S)?"`)
+	match := reg.FindStringSubmatch(codeLine)
+	groupNames := reg.SubexpNames()
+
+	remoteLib :=""
+	// 匹配远程仓库资源
+	if len(match)>0{
+		for i, name := range groupNames {
+			if i != 0 && name == "remoteLib" {
+				remoteLib = match[i]
+				break
+			}
+		}
+		if remoteRepo != "" {
+			remoteLibSplit := strings.Split(remoteLib,"/")[0:3]
+			return strings.Join(remoteLibSplit,"/");
+		}
+	}
+
+	return ""
+}
+// 调用 go get [remoteLib] 命令
+// 例如： go get github.com/snail007/go-activerecord
+func goget(remoteLib string) {
+	fmt.Println("go get " + remoteLib)
+	// 一下代码执行有些问题，先按上面打出来的命令手动执行吧
+	//cmd := exec.Command("go get " + remoteLib)
+	//cmd.Stdout = os.Stdout //
+	//err := cmd.Run()
+	//if err != nil {
+	//	fmt.Println("go get ",remoteLib," error.\n",err)
+	//	return
+	//}
+	//fmt.Println("go get ",remoteLib," success.")
+}
